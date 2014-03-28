935b56861db2789adcbfcb10ef1f700e87c796ec
barnstricolor <barnstricolor@gmail.com>
2014-03-28 14:28:35 -0300

    Controller cidade

--- IU/Controllers/CidadeController.cs
+++ IU/Controllers/CidadeController.cs
@@ -3,20 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using System.Web.Http;
 using AlphaNet.Application;
 
 namespace AlphaNet.IU.Controllers
 {
-    public class CidadeController : ApiController
+    [HandleError]
+    public class CidadeController : Controller
     {
         //
         // GET: /Cidade/
-        static readonly AviaoService aviaoService = new AviaoService();
-        
-        [System.Web.Http.HttpGet]
-        public string teste() {
-            return aviaoService.teste("xxxxx");
+        private AviaoService aviaoService = new AviaoService();
+
+    
+        public ActionResult Index()
+        {
+            return View();
         }
+        public ActionResult Nova()
+        {
+            return View("Form");
+        }
+
+        
     }
 }
