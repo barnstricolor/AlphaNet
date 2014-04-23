c50217894099a66a3943b7d6a164b462b1fccfdd
barnstricolor <barnstricolor@gmail.com>
2014-04-23 12:29:58 -0300

    Atualizacao IU

--- IU/Controllers/CidadeController.cs
+++ IU/Controllers/CidadeController.cs
@@ -20,5 +20,40 @@ namespace IU.Controllers
             return View(cidadeService.todasCidades());
         }
 
+        public ActionResult Nova() {
+            return View("Form", new CidadeData());
+        }
+
+        [HttpPost]
+        public ActionResult Salvar(CidadeData cidade) {
+            CidadeService cidadeService = new CidadeService();
+            if (cidade.cidadeId == null)
+            {
+                cidadeService.novaCidade(cidade.nome, cidade.cep);
+            }
+            else {
+                cidadeService.alterarDados(cidade.cidadeId, cidade.nome, cidade.cep);
+            }
+            return RedirectToAction("Index", "Cidade");
+        }
+
+        public ActionResult Editar(string cidadeId = "")
+        {
+            CidadeService cidadeService = new CidadeService();
+            CidadeData cidadeData = cidadeService.obterCidade(cidadeId);
+            return View("Form", cidadeData);
+        }
+
+        public ActionResult Excluir(string cidadeId = "")
+        {
+            CidadeService cidadeService = new CidadeService();
+            cidadeService.excluirCidade(cidadeId);
+            return RedirectToAction("Index", "Cidade");
+        }
+
+        protected override void OnException(ExceptionContext filterContext)
+        {
+            base.OnException(filterContext);
+        }
     }
 }
--- IU/Global.asax
+++ IU/Global.asax
@@ -1,1 +1,1 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AlphaNet.PassagemAerea.IU.WebApiApplication" Language="C#" %>
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="IU.WebApiApplication" Language="C#" %>
