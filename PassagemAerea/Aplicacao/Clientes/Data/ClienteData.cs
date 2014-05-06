1f51606dc8f06d04805c0cbfce0545ed0b1b713f
barnstricolor <barnstricolor@gmail.com>
2014-05-06 19:45:49 -0300

    Anotation e Testes de desconto em Reserva

--- PassagemAerea/Aplicacao/Clientes/Data/ClienteData.cs
+++ PassagemAerea/Aplicacao/Clientes/Data/ClienteData.cs
@@ -4,15 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using AlphaNet.PassagemAerea.Aplicacao.Cidades.Data;
+using System.ComponentModel.DataAnnotations;
 
 namespace AlphaNet.PassagemAerea.Aplicacao.Clientes.Data
 {
     public class ClienteData
     {
         public string clienteId {get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatório", AllowEmptyStrings = false)]
+        [RegularExpression("^[a-zA-ZãÃáÁàÀêÊéÉèÈíÍìÌôÔõÕóÓòÒúÚùÙûÛçÇºª' ']{1,60}$", ErrorMessage = "Este campo deve conter apenas letras")]
         public string nome {get; set; }
         public string email {get; set; }
         public string rg {get; set; }
+        
+        [Required(ErrorMessage = "Campo obrigatório", AllowEmptyStrings = false)]
+        [RegularExpression("^[0-9]{1,11}$", ErrorMessage = "Este campo deve conter apenas números")]
         public string cpf {get; set; }
 
         public string ocupacao {get; set; }
@@ -22,6 +29,8 @@ namespace AlphaNet.PassagemAerea.Aplicacao.Clientes.Data
         public bool promocao {get; set; }
         public bool especial {get; set; }
 
+        [Required(ErrorMessage = "Campo obrigatório", AllowEmptyStrings = false)]
+        [RegularExpression("^[0-9]{1,9}$", ErrorMessage = "Este campo deve conter apenas números")]
         public string telefone {get; set; }
         public string celular {get; set; }
 
@@ -29,6 +38,11 @@ namespace AlphaNet.PassagemAerea.Aplicacao.Clientes.Data
         public string numeroEndereco {get; set; }
         public string bairro {get; set; }
         public string cep {get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatório", AllowEmptyStrings = false)]
+        [DataType(DataType.Date, ErrorMessage = "Data inválida.")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        public string cadastro { get; set; }
         public CidadeData _cidade {get; set; }
 
     }
