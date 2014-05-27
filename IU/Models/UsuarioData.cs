5cb1a1319c70046b28b3de1e79a5e706d5853b7e
denisemcastro <denisemcastro@hotmail.com>
2014-05-27 20:48:08 -0300

    layout

--- IU/Models/UsuarioData.cs
+++ IU/Models/UsuarioData.cs
@@ -3,15 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace IU.Models
 {
     public class UsuarioData
     {  
         public string usuarioId { get; set; }
+
+        [Required(ErrorMessage = "O campo login é obrigatório")]
         public string login { get; set; }
+
+        [Required(ErrorMessage = "O campo nome é obrigatório")]
         public string nome { get; set; }
+
         public string email { get; set; }
+
+        [Required(ErrorMessage = "O campo senha é obrigatório")]
         public string senha { get; set; }
         public string papel { get; set; }
 //        public string cep { get; set; }
