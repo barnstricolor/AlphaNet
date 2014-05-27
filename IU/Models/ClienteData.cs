f915825344236eae9525aea8d33bbbe276ed31ef
barnstricolor <barnstricolor@gmail.com>
2014-05-27 16:02:33 -0300

    Takeda: tela cadastro clientes

--- IU/Models/ClienteData.cs
+++ IU/Models/ClienteData.cs
@@ -23,32 +23,41 @@ namespace IU.Models
         [Required(ErrorMessage = "Campo obrigatório", AllowEmptyStrings = false)]
         [RegularExpression("^[0-9]{1,11}$", ErrorMessage = "Este campo deve conter apenas números")]
         public string cpf {get; set; }
-
+        [Display(Name = "Ocupação")]
         [Required(ErrorMessage = "Campo obrigatório", AllowEmptyStrings = false)]
         public string ocupacao {get; set; }
-
+        [Display(Name = "Renda")]
         [Required(ErrorMessage= "Campo obrigatório")]
         public double renda {get; set; }
 
-        
+        [Display(Name = "Sexo")]
         public string sexo {get; set; }
+        [Display(Name = "Desconto de cliente especial")]
         public double desconto {get; set; }
+        [Display(Name = "Receber promoções?")]
         public bool promocao {get; set; }
+        [Display(Name = "Cliente Especial")]
         public bool especial {get; set; }
 
+        [Display(Name = "Telefone")]
         [Required(ErrorMessage = "Campo obrigatório", AllowEmptyStrings = false)]
         [RegularExpression("^[0-9]{1,9}$", ErrorMessage = "Este campo deve conter apenas números")]
         public string telefone {get; set; }
+        [Display(Name = "Celular")]
         public string celular {get; set; }
-
+        [Display(Name = "Endereço")]
         public string endereco {get; set; }
+        [Display(Name = "Nº")]
         public string numeroEndereco {get; set; }
+        [Display(Name = "Bairro")]
         public string bairro {get; set; }
+        [Display(Name = "C.E.P.:")]
         public string cep {get; set; }
 
+        [Display(Name = "Data de Cadastro")]
         [Required(ErrorMessage = "Campo obrigatório", AllowEmptyStrings = false)]
-        [DataType(DataType.Date, ErrorMessage = "Data inválida.")]
-        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:yyyy-MM-dd}")]
+        [DataType(DataType.Date, ErrorMessage = "Data inválida.")]        
         public DateTime dataCadastro { get; set; }
         public string cidade {get; set; }
 
