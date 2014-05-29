70227bcbc90505d16653f5478e1b068c160857a9
denisemcastro <denisemcastro@hotmail.com>
2014-05-29 03:41:05 -0300

    form e login

--- IU/Models/Contato.cs
+++ IU/Models/Contato.cs
@@ -9,18 +9,30 @@ namespace AlphaNet.PassagemAerea.IU.Models
     public class Contato
     {
         [Required(ErrorMessage = "O campo NOME é obrigatório!", AllowEmptyStrings = false )]
-        [Display(Name = "Nome", Prompt = "Nome")]
+        [Display(Name = "Nome")]
         public string nome { get; set; }
+
         [Required(ErrorMessage = "O campo E-MAIL é obrigatório!", AllowEmptyStrings = false)]
+        [Display(Name = "E-mail")]
         public string email { get; set; }
+
         [Required(ErrorMessage = "O campo TELEFONE é obrigatório!", AllowEmptyStrings = false)]
+        [Display(Name = "Telefone")]
         public string telefone { get; set; }
+
         [Required(ErrorMessage = "O campo CIDADE é obrigatório!", AllowEmptyStrings = false)]
+        [Display(Name = "Cidade")]
         public string cidade { get; set; }
+
         [Required(ErrorMessage = "O campo UF é obrigatório!", AllowEmptyStrings = false)]
+        [Display(Name = "UF")]
         public string uf { get; set; }
+
         [Required(ErrorMessage = "O campo ASSUNTO é obrigatório!", AllowEmptyStrings = false)]
+        [Display(Name = "Assnto")]
         public string assunto { get; set; }
+
+        [Display(Name = "Observação")]
         public string observacao { get; set; }
 
 
